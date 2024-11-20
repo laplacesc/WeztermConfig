0fe72d98ac932fa17b149b9a433c6a2de120d33f
laplacesc <50616351@qq.com>
2024-11-20 14:24:05 +0800

    wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,16 +1,59 @@
-local Config = require('config')
+local wezterm = require("wezterm")
+local mux = wezterm.mux
 
-require('utils.backdrops'):set_files():random()
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
 
-require('events.right-status').setup()
-require('events.left-status').setup()
-require('events.tab-title').setup()
-require('events.new-tab-button').setup()
+config = wezterm.config_builder()
 
-return Config:init()
-             :append(require('config.appearance'))
-             :append(require('config.bindings'))
-             :append(require('config.domains'))
-             :append(require('config.fonts'))
-             :append(require('config.general'))
-             :append(require('config.launch')).options
+config = {
+    -- 自动加载配置
+    automatically_reload_config = true,
+    -- 关闭标签栏
+    enable_tab_bar = false,
+    -- 关闭窗口时无需确认
+    window_close_confirmation = "NeverPrompt",
+    -- 移除顶部三个图标的栏，但仍可以调整大小
+    window_decorations = "RESIZE",
+    -- 光标设为闪烁条形
+    default_cursor_style = "BlinkingBar",
+    -- 主题设置
+    color_scheme = "Nord (Gogh)",
+    -- 字体设置
+    font_dirs = { 'fonts' },
+    font_locator = 'ConfigDirsOnly',
+    font = wezterm.font_with_fallback {
+        -- { family = "Operator Mono SSm", weight = "DemiLight", stretch = "Normal", style = "Italic" },
+        { family = "Sarasa Term SC Nerd", weight = "DemiBold", stretch = "Normal", style = "Italic" },
+    },
+    font_size = 14,
+    background = {
+        {
+            source = {
+                Color = "#282c35",
+            },
+            width = "100%",
+            height = "100%",
+            opacity = 0.8,
+        }
+    },
+    window_padding = {
+        left = 3,
+        right = 3,
+        top = 0,
+        bottom = 0,
+    },
+}
+
+config.keys = {
+    { key = 'E', mods = 'CTRL|SHIFT', action = wezterm.action.ShowTabNavigator },
+    { key = 'Enter', mods = 'ALT', action = wezterm.action.TogglePaneZoomState },
+    { key = 'w', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+    { key = 'w', mods = 'ALT', action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+    { key = 'v', mods = 'ALT|SHIFT', action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = 'h', mods = 'ALT|SHIFT', action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+}
+
+return config
