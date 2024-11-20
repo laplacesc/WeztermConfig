a996a1d42fe03aedb9440c6561cc1ebcf317136e
laplacesc <50616351@qq.com>
2024-11-20 15:03:02 +0800

    wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,4 @@
 local wezterm = require("wezterm")
-local mux = wezterm.mux
-
---wezterm.on('gui-startup', function(cmd)
---    local tab, pane, window = mux.spawn_window(cmd or {})
---    window:gui_window():maximize()
---end)
 
 config = wezterm.config_builder()
 
